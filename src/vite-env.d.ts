e5debf1c40d1fdd23081ac82d646be68df3c0ea7
大矢龍 <ohyaryo@ooyaryuunoMacBook-Air.local>
2025-07-07 11:26:54 +0900

    Fix TypeScript configuration and import.meta.env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly PROD: boolean
+  readonly DEV: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
